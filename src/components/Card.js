a6b387a1236b735ecb01e3f71868c43941e6d6f0
maxg29 <maxime_gagnon@hotmail.com>
2018-07-23 14:35:33 -0400

    Update Card.js
    fix

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,14 +4,14 @@ removeCard() {
     this.props.removeItem(this.props.id);
 }
 
-const Card = ({ name, email, id }) => {
+const Card = ({ name, email, id, removeItem }) => {
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
             <img alt='robotphoto' src={`https://robohash.org/${name}?200x200`} />
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
-                <button type="button" className="close" onClick={this.removeCard}>&times;</button>
+                <button type='button' className='close' onClick={this.removeCard}>&times;</button>
             </div>
         </div>
     );
